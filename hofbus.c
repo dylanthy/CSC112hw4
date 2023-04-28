b2abbf633fb9adfb77aa0c8390d36d92d9bed63f
dylanthy <mdfdylanyt@gmail.com>
2023-04-28 17:47:18 -0400

    modified hofbus.c file for better commente

--- hofbus.c
+++ hofbus.c
@@ -3,25 +3,25 @@
 #include <stdlib.h>
 
 struct station {
-	// write your code here
-	int free_seats; // Number of free seats on the bus
-  	int waiting_students; // Number of waiting students
-  	int next_ticket; // The ticket number of the next student to board
-  	int ticket_counter; // The counter for assigning the next ticket number
-  	pthread_mutex_t lock; // Mutex lock for synchronization
-  	pthread_cond_t bus_arrived; // Condition variable for bus arrival
-  	pthread_cond_t student_boarded; // Condition variable for student boarding
+	int available_spots; //on bus
+  	int next; //Next available student's ticket to board
+  	int counter;//counter that gives tickets
+  	int students;//amount waiting
+  	//pthread vars
+  	pthread_mutex_t lock; //mutex lock for synchronization, at start and end of load() and wait()
+  	pthread_cond_t bus_arrived; //conditional for bus arrival 
+  	pthread_cond_t student_boarded; //dconditional for boarding
 
 };
 
 void
 station_init(struct station *station)
 {
-	// write your code here
-	station->free_seats = 0;
-  	station->waiting_students = 0;
-  	station->next_ticket = 1;
-  	station->ticket_counter = 1;
+	//initializing station
+	station->students = 0;
+	station->available_spots = 0;
+  	station->next= 1;
+  	station->counter = 1;
   	pthread_mutex_init(&station->lock, NULL);
   	pthread_cond_init(&station->bus_arrived, NULL);
   	pthread_cond_init(&station->student_boarded, NULL);
@@ -30,49 +30,47 @@ station_init(struct station *station)
 void
 station_load_bus(struct station *station, int count)
 {
-	// write your code here
-	pthread_mutex_lock(&station->lock);
+	pthread_mutex_lock(&station->lock);//lock station
 
-  	// If there are no waiting students or the bus has no free seats, the bus leaves
-  	if (station->waiting_students == 0 || count == 0) {
+  	//Bus leaves if no spots or waiters
+  	if (station->students == 0 || count == 0) {
     	pthread_mutex_unlock(&station->lock);
     	return;
   	}
 
-  	// Update the number of free seats and notify the waiting students
-  	station->free_seats = count;
+  	//Update available seats and notify students through pthread
+  	station->available_spots = count;
   	pthread_cond_broadcast(&station->bus_arrived);
 
-  	// Wait until all free seats are taken or all waiting students have boarded
-  	while (station->free_seats > 0 && station->waiting_students > 0) {
+  	//Hold bus until no spots or waiters
+  	while (station->available_spots > 0 && station->students > 0) {
 		pthread_cond_wait(&station->student_boarded, &station->lock);
   	}
 
-  	// Reset the number of free seats and allow the bus to leave
-  	station->free_seats = 0;
-  	pthread_mutex_unlock(&station->lock);
+  	// Update free spots and let bus leave
+  	station->available_spots = 0;
+  	pthread_mutex_unlock(&station->lock);//unlock station
 }
 
 int
 station_wait_for_bus(struct station *station, int myticket, int myid)
 {
-	// write your code here
-	pthread_mutex_lock(&station->lock);
-  	station->waiting_students++;
+	pthread_mutex_lock(&station->lock);//lock station
+  	station->students++;
 
-  	// Wait until the bus arrives and it's the student's turn to board
-  	while (myticket != station->next_ticket || station->free_seats == 0) {
+  	//wait for arrival and boarding
+  	while (myticket != station->next|| station->available_spots== 0) {
     	pthread_cond_wait(&station->bus_arrived, &station->lock);
   	}
 
-  	// Board the bus and update the station state
-  	station->waiting_students--;
-  	station->next_ticket++;
-  	station->free_seats--;
+  	//fill bus and update station
+  	station->students --;
+  	station->next ++;
+  	station->available_spots --;
 
-  	// Notify the bus that a student has boarded
+  	//signal bus that student has boarded
   	pthread_cond_signal(&station->student_boarded);
-  	pthread_mutex_unlock(&station->lock);
+  	pthread_mutex_unlock(&station->lock);//unlock station
 
   	return (myticket);
 }
